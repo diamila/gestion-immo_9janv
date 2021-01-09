826ba282387d3eef791feb9a12b4f490e1ba9581
root <diamila>
2021-01-09 15:49:19 +0000

    first commit

---
+++ gestion_immobiliere/controllers/controllers.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from odoo import http
+from odoo.http import request
+from odoo.addons.website_sale.controllers.main import WebsiteSale
+import json
+
+
+from odoo import http, SUPERUSER_ID
+
+
+
+
+
+
+class Todo(http.Controller):
+
+    @http.route('/hello/boutique', website=True, auth='user')
+    def hello(self, **kw):
+        product_variant = request.env['product.product'].browse('product_id')
+        return request.render("gestion_immobiliere.hello")
+
+class Main(http.Controller):
+    @http.route('/todo', website=True, auth='user')
+    def index(self, **kw):
+        products = request.env['product.product'].sudo().search([('name_adresse_t', "ilike", self)], order="chambres asc")
+        return request.render("gestion_immobiliere.index", {'product': products})
+
+       
+class Main_detail(http.Controller):
+    @http.route('/todo/<model("product.product"):task>', website=True)
+    def index(self, task, **kw):
+        return http.request.render('gestion_immobiliere.detail',{'product': task})
+
+
+
